f659b8a1f61f8331ca487ff9a42abc0de0d72123
Xylo <89810131+xylorb@users.noreply.github.com>
2023-04-07 10:20:12 -0400

    basic version working on both local dev and remote hosted on github pages ...

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@ var APP_PREFIX = 'ttrk_';
 // you need to change this version (version_01, version_02…). 
 // If you don't change the version, the service worker will give your
 // users the old files!
-var VERSION = 'version_97';
+var VERSION = 'version_98';
 
 // The files to make available for offline use. make sure to add 
 // others to this list
